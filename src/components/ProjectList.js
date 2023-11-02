e724a2e6fa6a2a5c3cb8253b29b7373b168c22a2
anisamuema <anisa.muema.school@gmail.com>
2023-11-02 21:22:00 +0300

    update app

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import user from "../data/user";
 
-function ProjectList({ projects }) {
+function ProjectList({ projects, technologies }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <ProjectItem projects={user.projects} key={user.id}/>
+        <p>{technologies}</p>
+      </div>
     </div>
   );
 }
